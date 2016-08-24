4a554d832b2912f9a16b563b7e4e5514faa685a3
viviweiwei <webemailwei@163.com>
2016-08-24 19:37:40 +0800

    8月24号HWW

--- car-admin/src/main/webapp/admin/views/article/js/editContent.js
+++ car-admin/src/main/webapp/admin/views/article/js/editContent.js
@@ -1,4 +1,5 @@
   var edit=function(m){
 	  var id=m.getAttribute("datedit");
-	  window.open("../admin2/admin/views/article/articleEdit.html?id="+id);
+	  window.open("../admin2/admin/views/article/umeditor/_examples/completeDemo.html?id="+id);
+	  
   }
