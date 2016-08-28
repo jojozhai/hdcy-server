f6bead511fc8802af0ba6ed1306f544f5f14bead
yangruizss <yangrui@yangruideMBP.lan>
2016-08-28 13:01:35 +0800

    全局service,filter

--- car-admin/src/main/webapp/scripts/app.js
+++ car-admin/src/main/webapp/scripts/app.js
@@ -8,9 +8,25 @@
  * Main module of the application.
  */
 // 应用主模块
-angular.module('adminApp', [ 'admin',
-// 'voteAdminModule',
-'lotteryAdminModule', 'userAdminModule', 'buttonAdminModule', 'tagAdminModule',
-		'activityAdminModule', 'votingAdminModule', 'contraryAdminModule',
-		'leaderAdminModule', 'giftAdminModule', 'carAdminModule',
-		'posterAdminModule', 'articleAdminModule', 'commentAdminModule', 'yr' ]);
+angular.module(
+		'adminApp',
+		[ 'admin',
+		// 'voteAdminModule',
+		'lotteryAdminModule', 'userAdminModule', 'buttonAdminModule',
+				'tagAdminModule', 'activityAdminModule', 'votingAdminModule',
+				'contraryAdminModule', 'leaderAdminModule', 'giftAdminModule',
+				'carAdminModule', 'posterAdminModule', 'articleAdminModule',
+				'commentAdminModule', 'yr' ])
+.service("sponsorRestService",function($resource) {
+	return $resource("sponsor");
+}).service("customerServiceService", function($resource) {
+	return $resource("customerService");
+}).filter("enable", function() {
+	return function(text) {
+		if (text) {
+			return "下线";
+		}
+		return "上线";
+	}
+});
+;
