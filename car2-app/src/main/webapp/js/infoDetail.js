7322006a8f85b7657a575567496ebcebb9941a24
WIN8-1609011601\Administrator <webemailwei@163.com>
2016-09-18 10:23:32 +0800

    活动详情，礼物详情，大咖列表及详情

--- car2-app/src/main/webapp/js/infoDetail.js
+++ car2-app/src/main/webapp/js/infoDetail.js
@@ -18,11 +18,10 @@
 	        }
 	        return format;
 	    };
-	    
-        //自定义滚动轴    	         
-         //获取传递过来的id值，和类别的id         
-           function GetRequest() {        	
-                var url = location.search; 
+
+         //获取传递过来的id值，和类别的id
+           function GetRequest() {
+                var url = location.search;
                 var theRequest = new Object();
                 if (url.indexOf("?") != -1) {
                     var str = url.substr(1);
@@ -35,29 +34,27 @@
             }
             var Request = new Object();
             Request = GetRequest();
-            var id=Request.id;                   
-         	//var id=1026;        	                 
-         // 发起网路请求
-         // 创建请求对象
+            var show=Request.show;
+            var id=Request.id;
          var request = new XMLHttpRequest();
          // 检测请求状态
          request.onreadystatechange = function () {
          if (request.readyState == 4 && request.status == 200) {
-          // 获取数据
-          	 var jsonStr = request.responseText;
+          	var jsonStr = request.responseText;
           // 将json转为对象
-         	 var jsonObj = JSON.parse(jsonStr);                  
+         	var jsonObj = JSON.parse(jsonStr);
          	var tagInfos=jsonObj.tagInfos
          	var tagInfosName=function(){
         	for (var i = 0; i < jsonObj.tagInfos.length; i++) {
         		return tagInfos[i]['name'];
          	}
          }
-         var tagName=tagInfosName();               
+         var tagName=tagInfosName();
          var date = new Date(jsonObj.enableDate);
-         var timess=date.Format(); 
+         var timess=date.Format();
          var timeArr=timess.split("-");
          var today=Math.round(new Date().getTime());
+         console.log(today);
          var todayArr=new Date(today).Format().split("-");
          for (var i = 0; i < timeArr.length; i++) {
              if (timeArr[0]==todayArr[0]) {
@@ -72,7 +69,7 @@
                  }
 
              }
-         }         
+         }
          var newDiv=$("<div class='header'>\
                     	<h1>"+jsonObj.title+"</h1>\
                    		 <div class='times'>\
@@ -85,33 +82,34 @@
                       <div id='conDetail'>\
                            <article class='content'>"+jsonObj.content+"</article>\
                       </div>")
-                     $(".container").append(newDiv)                     
+                     $(".container").append(newDiv)
                      for (var i = 0; i < jsonObj.tagInfos.length; i++) {
-						tagInfos[i]['name'];	
+						tagInfos[i]['name'];
 						var spans=$("<span class='sort'>"+tagInfos[i]['name']+"</span>");
 						$(".sorts").append(spans);
 					}
                      var heights=document.documentElement.clientHeight;//获取设备的高度
-     	   			$("#conDetail").height(heights-682);
-     	   			var myScroll1 = new IScroll("#conDetail", {
-						hScroll: false,
-						vScroll: false,
-						bounce: false,
-						mouseWheel:true,
-						scrollbars:false,
-						checkDOMChanges: true,
-						iScrollBothScrollbarsH:true,
-						HWCompositing:true,
-						interactiveScrollbars:true,
-						fadeScrollbars:false,
-					});
-                           
+                     $(".container").height(heights-49);
+
                   }
               };
                // 使用请求对象，创建请求
                request.open("get", "../app2/article/"+id);
                // 发送请求
-               request.send();         
-              
+               request.send();
+               /*转发功能*/
+               if (show=="YES") {
+            	   $(".comment").show();
+            	   $(".comment input").on("focus",function(){
+            		   $(".erweima").show();
+            	   })
+
+               }
+              $(".erweima").on("click",function(){
+            	  $(".erweima").hide();
+              })
+
+
+
 
     	})
