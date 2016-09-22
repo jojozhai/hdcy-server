aa8c96b82638adb820e17cd23ba91cd5f25a526e
WIN8-1609011601\Administrator <webemailwei@163.com>
2016-09-22 10:34:59 +0800

    视频详情方法名修改

--- car2-core/src/main/resources/resources/admin/views/js/videoeditContent.js
+++ car2-core/src/main/resources/resources/admin/views/js/videoeditContent.js
@@ -1,5 +1,5 @@
-  var edit=function(m){
+  var videoEdit=function(m){
 	  var id=m.getAttribute("datedit");
 	  window.open("../admin2/admin/views/umeditor/_examples/completeDemo.html?id="+id);
-	  
+
   }
