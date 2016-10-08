b816be9b15bde3ef7bdde5c835d90f70c3fca647
zhailiang <zhailiang@haoduocheyou.com>
2016-10-08 10:09:02 +0800

    修改服务调用地址为线上环境地址

--- car2-core/src/main/java/com/ymt/mirage/car/domain/Activity.java
+++ car2-core/src/main/java/com/ymt/mirage/car/domain/Activity.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
