8c82cf55fae343dc4e9d7df8e92498d8152fc2ca
WIN8-1609011601\Administrator <webemailwei@163.com>
2016-10-09 15:19:05 +0800

    新版样式

--- hdcy-weixin-angular/src/app/comment/comment-all.component.ts
+++ hdcy-weixin-angular/src/app/comment/comment-all.component.ts
@@ -9,7 +9,7 @@ import {CommentService} from "./comment.service";
 @Component({
   selector: 'comment-all',
   templateUrl: './comment-all.component.html',
-  styleUrls: ['./comment-all.component.css']
+  styleUrls: ['./comment-list.component.css']
 
 })
 export class CommentAllComponent extends ListComponent implements OnInit {
