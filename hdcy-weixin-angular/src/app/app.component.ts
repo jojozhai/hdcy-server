acbccd6bd559864753d1f7d0a19e0b947648b4bd
zhailiang <zhailiang@haoduocheyou.com>
2016-10-10 14:59:57 +0800

    去掉wx

--- hdcy-weixin-angular/src/app/app.component.ts
+++ hdcy-weixin-angular/src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
 
   showFooter: boolean = false;
 
-  private wx = require('weixin-js-sdk');
+  // private wx = require('weixin-js-sdk');
 
   private showNavPaths: Array<string> = ['/video','/article','/activity','/leader','/my', '/'];
 
@@ -23,20 +23,20 @@ export class AppComponent {
       });
 
 
-    this.wx.config({
-      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-      appId: '', // 必填，公众号的唯一标识
-      timestamp: 1, // 必填，生成签名的时间戳
-      nonceStr: '', // 必填，生成签名的随机串
-      signature: '',// 必填，签名，见附录1
-      jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-    });
-
-    this.wx.ready(function(){
-      console.log("xixi");
-    });
-    this.wx.error(function(res){
-      console.log(res);
-    });
+    // this.wx.config({
+    //   debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    //   appId: '', // 必填，公众号的唯一标识
+    //   timestamp: 1, // 必填，生成签名的时间戳
+    //   nonceStr: '', // 必填，生成签名的随机串
+    //   signature: '',// 必填，签名，见附录1
+    //   jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+    // });
+    //
+    // this.wx.ready(function(){
+    //   console.log("xixi");
+    // });
+    // this.wx.error(function(res){
+    //   console.log(res);
+    // });
   }
 }
