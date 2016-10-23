1de35ce83d5be64f7a2ab8aabbee3da6206a0a1c
zhailiang <zhailiang@haoduocheyou.com>
2016-10-23 11:05:20 +0800

    日期时间处理

--- hdcy-weixin-angular/src/app/comment/comment-input.component.ts
+++ hdcy-weixin-angular/src/app/comment/comment-input.component.ts
@@ -4,64 +4,75 @@
 import {Component, OnInit} from "@angular/core";
 import {CommentService} from "./comment.service";
 import {ActivatedRoute} from "@angular/router";
-import {HttpRestService} from "../shared/service/http-rest.service";
 import {environment} from "../../environments/environment";
 
 @Component({
-  selector: 'comment-input',
-  templateUrl: './comment-input.component.html',
-  styleUrls: ['./comment.module.css']
+    selector: 'comment-input',
+    templateUrl: './comment-input.component.html',
+    styleUrls: ['./comment.module.css']
 })
 export class CommentInputComponent implements OnInit {
 
-  private defaultContent: string = '写点什么吧......';
+    private defaultContent: string = '写点什么吧......';
 
-  comment: string = this.defaultContent;
+    comment: string = this.defaultContent;
 
-  target: string;
+    target: string;
 
-  targetId: number;
+    targetId: number;
 
-  replyToId: number;
+    replyToId: number;
 
-  constructor(route: ActivatedRoute, private commentService: CommentService) {
-    this.target = route.snapshot.queryParams['target'];
-    this.targetId = route.snapshot.queryParams['targetId'];
-    this.replyToId = route.snapshot.queryParams['replyToId'];
-    if (!environment.userToken) {
-      commentService.login();
+    title: string;
+
+    constructor(route: ActivatedRoute, private commentService: CommentService) {
+        if (!environment.userToken) {
+            commentService.login();
+        }
+        this.target = route.snapshot.queryParams['target'];
+        this.targetId = route.snapshot.queryParams['targetId'];
+        this.replyToId = route.snapshot.queryParams['replyToId'];
+        this.title = route.snapshot.queryParams['title'];
+        if(this.replyToId) {
+            this.title = "写回复";
+        }else{
+            if (this.target == 'activity') {
+                this.title = '活动咨询';
+            } else {
+                this.title = '写评论';
+            }
+        }
     }
-  }
 
-  ngOnInit() {
+    ngOnInit() {
 
-  }
-
-  saveComment() {
-    this.commentService.create({
-      target: this.target,
-      targetId: this.targetId,
-      replyToId: this.replyToId,
-      content: this.comment
-    }, () => {
-      this.comment = this.defaultContent;
-      this.back();
-      if(this.target == 'activity') {
-        toastr.success('咨询信息提交成功');
-      }else{
-        toastr.success('评论发布成功');
-      }
-    });
-  }
-
-  back() {
-    window.history.back();
-  }
-
-  cleanComment() {
-    if (this.comment == this.defaultContent) {
-      this.comment = '';
     }
-  }
+
+    saveComment() {
+        this.commentService.create({
+            target: this.target,
+            targetId: this.targetId,
+            replyToId: this.replyToId,
+            content: this.comment
+        }, () => {
+            this.comment = this.defaultContent;
+            this.back();
+            if (this.target == 'activity') {
+                toastr.success('咨询信息提交成功');
+            } else {
+                toastr.success('评论发布成功');
+            }
+        });
+    }
+
+    back() {
+        window.history.back();
+    }
+
+    cleanComment() {
+        if (this.comment == this.defaultContent) {
+            this.comment = '';
+        }
+    }
 
 }
