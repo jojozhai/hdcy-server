2accdd9e88fb04ae4458f1be764746ea2ab83ab3
WIN8-1609011601\Administrator <webemailwei@163.com>
2016-10-26 10:48:48 +0800

    新版

--- hdcy-weixin-angular/src/app/my/my-info.component.ts
+++ hdcy-weixin-angular/src/app/my/my-info.component.ts
@@ -14,14 +14,15 @@ export class MyInfoComponent implements OnInit {
     user;
 	detailboxHeight: number = document.body.clientHeight - 48;
     constructor(private userService: UserService) {
-
+		
     }
 
     ngOnInit() {
         this.userService.getCurrentUserInfo().subscribe(
             res => this.user = res.json(),
             err => this.userService.handleException(err)
-        );
+                   
     }
+    
 
 }
