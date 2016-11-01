97ed92f9e8e6486b473aeb9df380e190fa778e89
WIN8-1609011601\Administrator <webemailwei@163.com>
2016-11-01 10:45:11 +0800

    新版

--- hdcy-weixin-angular/src/app/my/my-info.component.ts
+++ hdcy-weixin-angular/src/app/my/my-info.component.ts
@@ -17,6 +17,8 @@ export class MyInfoComponent implements OnInit {
   type; 
   closes;
   editbg;
+  year;
+  month;
   detailboxHeight: number = document.body.clientHeight - 48;
 
   constructor(private userService: UserService, private weixinService: WeixinService) {
@@ -33,14 +35,45 @@ export class MyInfoComponent implements OnInit {
       res => this.user = res.json(),
       err => this.userService.handleException(err)
     );
+//  城市编辑
+    $('body').on('click', '.city-list p', function () {
+		var type = $('.container').data('type');	
+		$('#zone_ids').html($(this).html()).attr('data-id',$(this).attr('data-id'));
+	//	$('body').html($(this).html()).attr('data-id', $(this).attr('data-id'));
+	
+	  console.log($(this).html());
+	  
+	  $('.container').hide();
+	  $('.editbg').hide()
+	});
+	$('body').on('click', '.letter a', function () {
+		var s = $(this).html();
+		$(window).scrollTop($('#' + s + '1').offset().top);
+	});
+	var newyear=(new Date()).getFullYear();	
+	for (var i=1970;i< newyear;i++) {
+		var ops=$("<option value="+i+"年>"+i+"年</option>");
+		$('.year').append(ops);
+	}
+	for (var j=1;j<=12;j++) {
+		if (j<10) {
+			var opsri=$("<option value=0"+j+"月>0"+j+"月</option>");
+		}else {
+			var opsri=$("<option value="+j+"月>"+j+"月</option>");
+		}		
+		$('.month').append(opsri);
+	}
+	
+	
   }
 
   uploadHeadImg() {
     this.weixinService.fileUpload();
   }
   
+   
 //个人资料
-  edit(type){
+  edit(type,year,month){
 	this.type=type;
 	if (this.type!='cars') {
 		this.editbg="block";
@@ -48,8 +81,32 @@ export class MyInfoComponent implements OnInit {
 	if (this.type=='nicks') {
 		console.log(this)
 	}
+	if (this.type=='citys') {
+		$('html,body').css({
+			height:'auto',
+			overflow: 'scroll',
+		})
+		
+	}
+	if (this.type=='births') {			
+		$(".datas .year").val($(".birthday").html().substring(0,5));
+		$(".datas .month").val($(".birthday").html().substring(5));
+	}
   }
-  
+//手机号
+nextstep() {
+	$(".step1").hide();
+	$(".step3").show();
+	$(".telphon-num input").val($(".tel-num input").val())
+	
+}
+//出生年月编辑
+birthSave() {
+	var year=$(".datas .year").val();
+	var month=$(".datas .month").val();
+	console.log(year+month)
+	
+}
 //昵称
   clearnick(){
   	$(".nicks input").val("");
@@ -58,11 +115,13 @@ export class MyInfoComponent implements OnInit {
   	$(".nicks input").val();
   	console.log($(".nicks input").val())
   }
+
 //车型
   close () {
 	this.type="none";
 	this.editbg="none";
   }
+   
   huoqu(){
   	this.type="none";
   	console.log(event.target.innerHTML);
@@ -79,6 +138,7 @@ export class MyInfoComponent implements OnInit {
   	this.num++;  	
   	console.log(event.target.innerHTML);
   }
+  
   
 
 }
