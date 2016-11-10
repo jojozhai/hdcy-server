6389eca6b30deb59f44fb51391c1363d70899a7a
zhailiang <zhailiang@haoduocheyou.com>
2016-11-10 21:12:14 +0800

    暂存

--- hdcy-weixin-angular/src/app/comment/comment-item.component.ts
+++ hdcy-weixin-angular/src/app/comment/comment-item.component.ts
@@ -4,6 +4,7 @@
 import {Component, OnInit, Input} from "@angular/core";
 import {PraiseService} from "../shared/service/praise.service";
 import {environment} from "../../environments/environment";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'comment-item',
@@ -15,44 +16,59 @@ export class CommentItemComponent implements OnInit {
   @Input() private comment;
 
   @Input() private styleType: string = "default";
-  
-  constructor(private praiseService: PraiseService) {
+
+  target: string;
+
+  targetId: any;
+
+  constructor(public route: ActivatedRoute, public router: Router, public praiseService: PraiseService) {
+    this.target = route.snapshot.queryParams['target'];
+    this.targetId = route.snapshot.queryParams['targetId'];
   }
-  flag=1;
+
+  flag = 1;
+
   ngOnInit() {
-  	
+
+  }
+
+  toReplyPage(id:number){
+    if(this.target && this.targetId) {
+      this.router.navigate(['/comment/input'], {queryParams: {target: this.target, targetId: this.targetId, replyToId: id}})
+    }
   }
 
   praise(comment) {
     this.praiseService.create({target: 'comment', targetId: comment.id}, function (res) {
       comment.praised = !comment.praised;
-      if(comment.praised){
+      if (comment.praised) {
         comment.praiseCount = comment.praiseCount + 1;
-      }else{
+      } else {
         comment.praiseCount = comment.praiseCount - 1;
       }
     });
   }
-   morcom() {   
-    	if (this.flag==1) {    		
-    		$(".commments .morecoms").css({
-    			height:'auto',
-    			'overflow-y': 'auto',    			
-    		});
-    		this.flag=2;
-    	}else {
-    		for (var i=0;i<$(".commments .morecoms").length;i++) {
-    			if ($(".commments .morecoms").eq(i).height()!=0) {
-    		 		$(".commments .com-more").prev().css({
-    					height:'55px',
-    					'overflow-y': 'hidden',
-    			
-    		 		});
-    			}
-    		}   		
-    		
-    	}
-	}
+
+  morcom() {
+    if (this.flag == 1) {
+      $(".commments .morecoms").css({
+        height: 'auto',
+        'overflow-y': 'auto',
+      });
+      this.flag = 2;
+    } else {
+      for (var i = 0; i < $(".commments .morecoms").length; i++) {
+        if ($(".commments .morecoms").eq(i).height() != 0) {
+          $(".commments .com-more").prev().css({
+            height: '55px',
+            'overflow-y': 'hidden',
+
+          });
+        }
+      }
+
+    }
+  }
 
 }
 
