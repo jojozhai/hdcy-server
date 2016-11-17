d36444bf472d9962fa4344c4674c7bad5aaaf20a
WIN8-1609011601\Administrator <webemailwei@163.com>
2016-11-17 17:48:55 +0800

    新版

--- hdcy-weixin-angular/src/app/activity/activity-sign.component.ts
+++ hdcy-weixin-angular/src/app/activity/activity-sign.component.ts
@@ -18,7 +18,9 @@ export class ActivitySignComponent implements OnInit {
   message: string = this.defaultContent;
 
   activityId: number;
-
+  detailboxHeight: number = document.body.clientHeight - 48;
+  realname:string;
+  mobile:number;
   constructor(route: ActivatedRoute, private activityService: ActivityService) {
     this.activityId = route.snapshot.queryParams['id'];
     if (!environment.userToken) {
@@ -31,7 +33,18 @@ export class ActivitySignComponent implements OnInit {
 
   sign() {
     if(environment.userToken){
-      this.activityService.sign({activityId: this.activityId, message: this.message})
+		if ($(".name-input").val()==""||$(".tel-input").val()=="") {			
+			alert("请填写完整信息");
+		} else{
+			if ($(".tel-input").val().length>11||$(".tel-input").val().length<11) {
+				alert("手机号位数不对");				
+			} else{
+//				realname:this.realname,mobile:this.mobile,
+				this.activityService.sign({activityId: this.activityId, message: this.message})
+			}
+			
+		}
+      
     }else{
       this.activityService.login();
     }
