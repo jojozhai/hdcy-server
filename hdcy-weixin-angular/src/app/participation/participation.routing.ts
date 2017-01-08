7764662c17e053db0ccae9904604760ee9fcaa4e
zhailiang <zhailiang@haoduocheyou.com>
2017-01-08 11:26:14 +0800

    删除无用项目

--- hdcy-weixin-angular/src/app/participation/participation.routing.ts
+++
@@ -1,9 +0,0 @@
-import {Routes, RouterModule} from "@angular/router";
-import {ModuleWithProviders} from "@angular/core";
-import {ParticipationListComponent} from "./participation-list.component";
-
-const participationRoutes: Routes = [
-    { path: 'participation', component: ParticipationListComponent }
-];
-
-export const participationRouting: ModuleWithProviders = RouterModule.forChild(participationRoutes);
