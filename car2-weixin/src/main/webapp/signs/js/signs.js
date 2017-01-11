527f75db27b1c7e178769d619a34ce681b21e90e
viviweiwei <webemailwei@163.com>
2017-01-11 13:15:14 +0800

    求签

---
+++ car2-weixin/src/main/webapp/signs/js/signs.js
@@ -0,0 +1,10 @@
+$(function () {
+  $(".enter").on('click',function () {
+    $(".page").hide();
+    $(".page1").show();
+  })
+  $(".man").on('click',function () {
+    $(".man").hide();
+    $(".man1").show();
+  })
+})
